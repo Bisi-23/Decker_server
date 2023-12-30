ba70bcf301c37d59ba8903f9d00cf196fea96501
Bisi-23 <adebiso23@gmail.com>
2023-12-29 22:41:13 -0600

    updated

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region = "us-east-1"
-  profile= "yusuf"
+  profile= "Bisif"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "yusuf-docker-tfstate"
+  bucket = "Bisi-docker-tfstate"
      
   lifecycle {
     prevent_destroy = true
